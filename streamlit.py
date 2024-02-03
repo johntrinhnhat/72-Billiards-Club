cf423c25cb73c4065af42d7af0c51a5b1820eb76
KHOITRINH03 <Johntrinhnhat@gmail.com>
2024-02-03 01:23:33 -0500

    Daily update

--- streamlit.py
+++ streamlit.py
@@ -34,8 +34,8 @@ print(df['Hour'].dtype)
 df_customer = load_customer_data()
 df_table = load_table_data()
 df_table['Date'] = pd.to_datetime(df['Date']).dt.date
-df_table['EntryHour'] = pd.to_datetime(df_table['EntryHour'], format='mixed').dt.time
-df_table['ExitHour'] = pd.to_datetime(df_table['ExitHour'], format='mixed').dt.time
+df_table['Check_In'] = pd.to_datetime(df_table['Check_In'], format='mixed').dt.time
+df_table['Check_Out'] = pd.to_datetime(df_table['Check_Out'], format='mixed').dt.time
 
 print(df_table.dtypes)
 # CSS styling
