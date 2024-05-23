b9d796890ced07c10a15fe874d4a3dc7a2bcc650
KHOITRINH03 <Johntrinhnhat@gmail.com>
2024-05-24 00:11:28 +0700

    Daily update

--- streamlit.py
+++ streamlit.py
@@ -301,35 +301,35 @@ with tab3:
 #     #     ax.set_ylabel("Date")
 #     #     st.pyplot(fig)
 
-with tab4: 
-    st.title("ChatGPT-like clone")
+# with tab4: 
+#     st.title("ChatGPT-like clone")
 
 
-    client = OpenAI(api_key= os.getenv("open_api_key"))
+#     client = OpenAI(api_key= os.getenv("open_api_key"))
 
-    if "openai_model" not in st.session_state:
-        st.session_state["openai_model"] = "gpt-4o"
+#     if "openai_model" not in st.session_state:
+#         st.session_state["openai_model"] = "gpt-4o"
 
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+#     if "messages" not in st.session_state:
+#         st.session_state.messages = []
 
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+#     for message in st.session_state.messages:
+#         with st.chat_message(message["role"]):
+#             st.markdown(message["content"])
 
-    if prompt := st.chat_input("What is up?"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+#     if prompt := st.chat_input("What is up?"):
+#         st.session_state.messages.append({"role": "user", "content": prompt})
+#         with st.chat_message("user"):
+#             st.markdown(prompt)
 
-        with st.chat_message("assistant"):
-            stream = client.chat.completions.create(
-                model=st.session_state["openai_model"],
-                messages=[
-                    {"role": m["role"], "content": m["content"]}
-                    for m in st.session_state.messages
-                ],
-                stream=True,
-            )
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+#         with st.chat_message("assistant"):
+#             stream = client.chat.completions.create(
+#                 model=st.session_state["openai_model"],
+#                 messages=[
+#                     {"role": m["role"], "content": m["content"]}
+#                     for m in st.session_state.messages
+#                 ],
+#                 stream=True,
+#             )
+#             response = st.write_stream(stream)
+#         st.session_state.messages.append({"role": "assistant", "content": response})
